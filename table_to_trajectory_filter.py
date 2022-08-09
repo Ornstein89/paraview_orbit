ab5cd0124f28392337978d932d2f05e769d49048
Anton Nedogarok <nk260an@gmail.com>
2022-08-09 16:50:15 +0300

    added detailed comments to table_to_trajectory_filter

--- table_to_trajectory_filter.py
+++ table_to_trajectory_filter.py
@@ -1,20 +1,31 @@
-print("inputs = ", inputs[0])
+# the single input table this filter is applied to
 table = inputs[0]
-print("table.GetNumberOfRows() = ", table.GetNumberOfRows())
-num_points = table.GetNumberOfRows()
-#print("table.GetValue() = ",table.GetValue(0,0))
-vtkpoints = vtk.vtkPoints()
-for i in range(0,num_points):
-    vtkpoints.InsertPoint(i,
-        table.GetValue(i,0).ToFloat(),
-        table.GetValue(i,1).ToFloat(),
-        table.GetValue(i,2).ToFloat())
 
-#print("GetNumberOfPoints = ", )
-output.SetPoints(vtkpoints) 
-output.Allocate(1, 1)
+# print table properties and metrics just for sure
+print("table: ", table)
+print("table.GetNumberOfRows() = ", table.GetNumberOfRows())
+
+num_rows = table.GetNumberOfRows()
+
+# usual vtk workflow: fill vtkPoints first
+vtkpoints = vtk.vtkPoints()
+for i in range(0, num_rows):
+    vtkpoints.InsertPoint(i,
+        table.GetValue(i,0).ToFloat(), #x column
+        table.GetValue(i,1).ToFloat(), #y column
+        table.GetValue(i,2).ToFloat()  #z column
+        )
+output.SetPoints(vtkpoints)
+
+# allocate vtkCell, representing single line
+# if more lines, use output.Allocate(N_OF_LINES, 1) 
+output.Allocate(1, 1) 
+
 vtkpolyline = vtk.vtkPolyLine()
-vtkpolyline.GetPointIds().SetNumberOfIds(num_points)
-for i in range(0,num_points):
+vtkpolyline.GetPointIds().SetNumberOfIds(num_rows)
+# enumerate points to include in polyline
+for i in range(0,num_rows):
     vtkpolyline.GetPointIds().SetId(i, i)
-output.InsertNextCell(vtkpolyline.GetCellType(), vtkpolyline.GetPointIds()) 
+# assign vtkPolyLine graphical object to created vtkCell
+output.InsertNextCell(vtkpolyline.GetCellType(),
+                      vtkpolyline.GetPointIds()) 
